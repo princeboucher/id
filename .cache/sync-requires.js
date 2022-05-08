0c3940f67cc8c16efdcb5cbf3905dfbe4ddcb77a
princeboucher <736187+princeboucher@users.noreply.github.com>
2022-05-07 21:33:56 -0700

    verbose command
    added verbose to the build command

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -4,7 +4,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/Users/zcoolx/Developer/id/.cache/dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("/Users/zcoolx/Developer/id/src/pages/404.js")),
   "component---src-pages-index-js": preferDefault(require("/Users/zcoolx/Developer/id/src/pages/index.js"))
 }
