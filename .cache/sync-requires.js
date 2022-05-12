c56c59e91c27852dfd1b15f3d329143378038ff0
princeboucher <736187+princeboucher@users.noreply.github.com>
2022-05-12 10:40:44 -0700

    build & develop push

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -4,6 +4,7 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/zcoolx/Developer/id/.cache/dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("/Users/zcoolx/Developer/id/src/pages/404.js")),
   "component---src-pages-index-js": preferDefault(require("/Users/zcoolx/Developer/id/src/pages/index.js"))
 }
